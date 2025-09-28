453228fee4ee72c8c92906b1e96296f57d2f3705
cherukurupavan <75001888+cherukurupavan@users.noreply.github.com>
2025-09-28 23:02:16 +0530

    Add files via upload

---
+++ src/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import portfolicoPage from './pages/portfolio'
+function Routes() {
+  return (
+    <div>
+      <portfolicoPage />
+    </div>
+  )
+}
+
+export default Routes
